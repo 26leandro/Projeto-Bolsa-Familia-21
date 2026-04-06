d2a7a87bfaa3b150724e05719a618ae7147672e5
26leandro <leandrochavesefg@gmail.com>
2026-04-06 19:30:32 -0300

    primeiro commit

---
+++ SRC/processamento.py
@@ -0,0 +1,137 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, regexp_replace,sum,avg,count,desc, round
+import matplotlib.pyplot as plt
+
+
+spark = SparkSession.builder \
+    .appName("Bolsa Familia") \
+    .config("spark.driver.memory", "8g")\
+    .config("spark.executor.memory", "8g")\
+    .config("spark.sql.suffle.partitions", "50")\
+    .getOrCreate()
+
+# Lendo o arquivo
+
+caminho_csv = "Dados/Pagamentos.csv"
+
+df = spark.read\
+.option("header",True )\
+.option("inferschema", True)\
+.option("sep",";")\
+.option("Encoding", "Iso-8859-1")\
+.csv(caminho_csv)
+
+# 1. Definição do dicionário
+
+colunas_padrao = {
+    "MÊS COMPETÊNCIA": "mes_competencia",
+    "MÊS REFERÊNCIA": "mes_referencia",
+    "UF": "uf",
+    "CÓDIGO MUNICÍPIO SIAFI": "codigo_municipio_siafi",
+    "NOME MUNICÍPIO": "nome_municipio",
+    "CPF FAVORECIDO": "cpf_favorecido",
+    "NIS FAVORECIDO": "nis_favorecido",
+    "NOME FAVORECIDO": "nome_favorecido",
+    "VALOR PARCELA": "valor_parcela"
+}
+
+# 2. Padronização de uma vez só partindo do df original
+
+df_tratado = df
+for antiga, nova in colunas_padrao.items():
+    df_tratado = df_tratado.withColumnRenamed(antiga, nova)
+
+#Tratamento Automático
+
+import unicodedata
+import re
+
+def padronizar_nome(col):
+    col = col.lower()
+    col = unicodedata.normalize("NFD", col)
+    col = col.encode("ascii", "ignore").decode("utf-8")
+    col = re.sub(r"[^a-z0-9]+", "_", col)
+    col = col.strip("_")
+    return col
+
+df_tratado =  df_tratado.toDF(
+    *[padronizar_nome(c) for c in df_tratado.columns]
+)
+
+
+# Trata Valores nulos de uma coluna da tabela expecifica.
+
+# -- df_tratado = df_tratado.dropna(subset=["valor_parcela"])-- 
+
+# Trata valores nulos da tabela inteira
+
+df_tratado = df_tratado.dropna()
+
+# Troca vírgula por ponto. 
+
+df_tratado = (
+    df_tratado
+    .dropna()
+    .withColumn(
+    "valor_parcela",
+    regexp_replace(col("valor_parcela"), "," , "."))
+    .withColumn(
+    "valor_parcela",
+    col("valor_parcela").cast("decimal(10,2)"))
+)
+
+
+# 
+
+df_tratado.agg(
+    round(sum("valor_parcela"), 2).alias("total_pago"),
+    round(avg("valor_parcela"), 2).alias("media_pagamento")    
+).show()
+
+#
+
+df_tratado.groupBy("uf")\
+    .agg(sum("valor_parcela").alias("total_pago"))\
+    .orderBy("total_pago", ascending=False)\
+    .show(27)
+
+#
+
+ranking_favorecido = df_tratado.groupBy("cpf_favorecido" , "nome_favorecido" )\
+    .agg(
+        sum("valor_parcela").alias("valor_total_acumulado"),
+        count("valor_parcela").alias("quantidade_parcelas")
+    )\
+    .orderBy(desc("valor_total_acumulado"))
+
+#
+
+print("Ranking dos 10 primeiros com valores acumulados")
+ranking_favorecido.show(30, truncate=False)
+
+#
+
+media_geral = df_tratado.agg(avg("valor_parcela")).collect()[0][0]
+print(f"Para fins de comparação, media geral de cada parcela e R$ {media_geral:.2f}")
+
+#
+
+df_uf = df_tratado.groupBy("uf") \
+    .agg(sum("valor_parcela").alias("total_pago")) \
+    .orderBy("total_pago", ascending=False) \
+    .limit(10) \
+    .toPandas()
+
+# Gráfico
+plt.figure()
+plt.bar(df_uf["uf"], df_uf["total_pago"])
+plt.title("Top 10 UFs - Total Pago Bolsa Família")
+plt.xlabel("UF")
+plt.ylabel("Total Pago")
+plt.show()
+
+# Exiba os resultados
+
+df_tratado.show(5)
+df_tratado.printSchema()
+
